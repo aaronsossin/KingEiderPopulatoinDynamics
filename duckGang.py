10a46ac5f989654ed394778bad570849a817d54b
Aaron Sossin <aaron.sossin@mail.mcgill.com>
2019-11-28 15:53:35 -0500

    Oil spiill functionality, comments

--- duckGang.py
+++ duckGang.py
@@ -2,22 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statistics import mean
 
-initialPopulation = 10000
 
-n1= int(initialPopulation/3)
+initialPopulation = 10000
+n1= int(initialPopulation/3) #node-1 of model
 n2 = int(initialPopulation/3)
 n3 = int(initialPopulation/3)
 
+oilSpill = 1000
+n3 = n3 - oilSpill
 
+S1 = 0.8 #survival rate from n1 -> n2
+S2 = S1 #survival rate from n2 -> n3
+S3 = 0.8 #survival rate from n3 -> n3
 
-S1 = 0.8
-S2 = S1
-S3 = 0.8
+F3 = 0.78 * 4.41 * 0.36 * 0.10 * 0.65 * 1 * 0.5 #fecundity rate
+print("Fecundity rate (doesn't make sense) ",  F3)
 
-F3 = 0.78 * 4.41 * 0.36 * 0.10 * 0.65 * 1 * 0.5
-print("F3 ",  F3)
+timeLapse = 50 #years
 
-timeLapse = 50
+""" Initialization """
 n1s = [n1]
 n2s = [n2]
 n3s = [n3]
@@ -25,29 +28,29 @@ pops = [initialPopulation]
 xs = [0]
 pop_loss = []
 tot = initialPopulation
+
+""" Running model """
 for x in range(timeLapse):
+    """ NODE UPDATES"""
     N2 = int(n1*S1)
     N3 = int(n3*S3 + n2*S2)
     N1 = int(n3*F3)
     totalPopulation = N2 + N3 + N1
+
+    """ FOR GRAPH"""
     xs.append(x)
     n1s.append(N1)
     n2s.append(N2)
     n3s.append(N3)
     pop_loss.append(int((totalPopulation - tot)/tot*100))
     pops.append(totalPopulation)
-    #print("Total Population ", totalPopulation)
-    """
-    
-    print("Year ", x + 1)
-    print("n1 ",N1)
-    print("n2 ", N2)
-    print("n3 ", N3)
-    """
 
+    """ STOP WHEN NO MORE EIDERS"""
     if (N1 < 1 and N2 < 1 and N3 < 1):
         print("YOU DEAD")
         break;
+
+    """ SAVE LAST ITERATION"""
     n1 = N1
     n2 = N2
     n3 = N3
